04429e7eda979251edfd92e0c34d14f74801b0cb
Mark Music <me@markmusic.io>
2025-06-29 12:39:00 -0600

    ft: load data and create visualization tool

---
+++ utils/data_loader.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import struct
+
+def load_images(path):
+    file = open(path, "rb")
+    
+    try:
+        magic, num, rows, cols = struct.unpack(">IIII", file.read(16))
+        
+        image_data = file.read()
+        images = np.frombuffer(image_data, dtype=np.uint8).reshape(num, rows * cols)
+        
+        return images / 255.0
+    finally:
+        file.close()
+        
+def load_labels(path):
+    file = open(path, "rb")
+    
+    try:
+        magic, num = struct.unpack(">II", file.read(8))
+        labels = np.frombuffer(file.read(), dtype=np.uint8)
+        
+        return labels
+    finally:
+        file.close()
+
+def load_data():
+    path = "data"
+    X_train = load_images(f"{path}/train-images.idx3-ubyte")
+    y_train = load_labels(f"{path}/train-labels.idx1-ubyte")
+    
+    X_test = load_images(f"{path}/train-images.idx3-ubyte")
+    y_test = load_labels(f"{path}/train-labels.idx1-ubyte")
+    
+    return X_train, y_train, X_test, y_test
+
+def visualize(X, y):
+    plt.imshow(X.reshape(28, 28), cmap="gray")
+    plt.title(f"Label: {y}")
+    plt.axis("off")
+    plt.show()
+    
+def one_hot_encode(y, num_classes=10):
+    return np.eye(num_classes)[y]
+    
